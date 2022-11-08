dfe187c24ee8ecaf4485d056454b26d63193f489
Mark Green <markosindustries@gmail.com>
2022-11-08 17:50:17 +1100

    Use GRPC Context rather than ThreadLocal to propagate remote address
    We shouldn't rely on the threads being used being the same, GRPC has a way to propagate information from interceptors

--- core/src/main/java/com/markosindustries/distroboy/core/clustering/ServerCallAddressInterceptor.java
+++ core/src/main/java/com/markosindustries/distroboy/core/clustering/ServerCallAddressInterceptor.java
@@ -2,6 +2,8 @@ package com.markosindustries.distroboy.core.clustering;
 
 import static java.util.Objects.nonNull;
 
+import io.grpc.Context;
+import io.grpc.Contexts;
 import io.grpc.ForwardingServerCallListener;
 import io.grpc.Grpc;
 import io.grpc.Metadata;
@@ -11,24 +13,26 @@ import io.grpc.ServerInterceptor;
 
 /** Provides the remote host address by intercepting GRPC headers */
 public class ServerCallAddressInterceptor implements ServerInterceptor, RemoteAddressProvider {
-  private final ThreadLocal<String> currentRequestClientAddress =
-      ThreadLocal.withInitial(() -> null);
+  private static final Context.Key<String> CLIENT_ADDRESS = Context.key("client-address");
 
   @Override
   public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
       ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
     final var remoteAddress = call.getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR);
     if (nonNull(remoteAddress)) {
-      currentRequestClientAddress.set(remoteAddress.toString());
-    } else {
-      currentRequestClientAddress.set(null);
+      return Contexts.interceptCall(
+          Context.current().withValue(CLIENT_ADDRESS, remoteAddress.toString()),
+          call,
+          headers,
+          next);
     }
+
     return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(
         next.startCall(call, headers)) {};
   }
 
   @Override
   public String getRemoteAddressOfCurrentRequest() {
-    return currentRequestClientAddress.get();
+    return CLIENT_ADDRESS.get(Context.current());
   }
 }
